a2d3314786e9b89f0bb2ed2a8b77a600c223d12c
braveup <784249422@qq.com>
2017-03-07 10:08:38 +0800

    文件写入换行

--- spring-core/src/test/java/studytest/IO/IoTest.java
+++ spring-core/src/test/java/studytest/IO/IoTest.java
@@ -2,12 +2,18 @@ package studytest.IO;
 
 import org.junit.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 /**
  * Created by braveup on 2017/3/5.
  */
 public class IoTest {
+    public static final String STRING = System.getProperty("line.separator");
+
     @Test
     public void testIo() {
         File dir = new File("E:\\xiangmu");
@@ -26,24 +32,28 @@ public class IoTest {
             }
         }
     }
+
     //文件流的作用是打开文件，在用字节流或者字符流将信息写入，最终关闭资源
     @Test
-    public  void  test() throws FileNotFoundException {
-        File  file=new File ("E://test");
-        if (!file.exists()){
+    public void test() throws FileNotFoundException {
+        File file = new File("E://test");
+
+        System.out.println( File.separator);
+        if (!file.exists()) {
             file.mkdir();
         }
-        FileOutputStream  fileOutputStream=new FileOutputStream("E://test/a.txt",true);
-        OutputStream  outputStream=new BufferedOutputStream(fileOutputStream);
-        //如何增量的写入
-        String str="hellow";
+        //如何增量的写入 true
+        FileOutputStream fileOutputStream = new FileOutputStream("E://test/a.txt", true);
+        // OutputStream  outputStream=new BufferedOutputStream(fileOutputStream);
+        //如何换行写入
+        String str = "hellow" + STRING;
         try {
-         outputStream.write(str.getBytes());
+            fileOutputStream.write(str.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
-                outputStream.close();
+                fileOutputStream.close();
             } catch (IOException e) {
                 new RuntimeException(e);
             }
